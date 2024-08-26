9ad7cb2a6ce92af62fb7e4c412caf37b0ec4348d
Vilar95 <e.vilar95@gmail.com>
2024-08-26 16:15:08 -0300

    Refatora Category com CategoryTransitionWidget;

--- lib/screens/components/category.dart
+++ lib/screens/components/category.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hyrule/controllers/api_controller.dart';
+import 'package:hyrule/screens/components/category_transition.dart';
 import 'package:hyrule/screens/results.dart';
 import 'package:hyrule/utils/consts/categories.dart';
 
@@ -15,42 +16,8 @@ class Category extends StatefulWidget {
   State<Category> createState() => _CategoryState();
 }
 
-class _CategoryState extends State<Category> with TickerProviderStateMixin {
+class _CategoryState extends State<Category> {
   final ApiController apiController = ApiController();
-
-  late AnimationController _scaleAnimationController;
-  late AnimationController _imageColorAnimationController;
-
-  @override
-  void initState() {
-    _scaleAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-      lowerBound: 0.8,
-      upperBound: 1,
-    );
-
-    _imageColorAnimationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-      lowerBound: 0,
-      upperBound: 1,
-    );
-
-    if (widget.isHighlight) {
-      _scaleAnimationController.repeat(
-        reverse: true,
-      );
-      _imageColorAnimationController.repeat(
-        reverse: true,
-      );
-    } else {
-      _scaleAnimationController.animateTo(1);
-      _imageColorAnimationController.animateTo(0);
-    }
-
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,45 +38,25 @@ class _CategoryState extends State<Category> with TickerProviderStateMixin {
             },
             borderRadius: BorderRadius.circular(16.0),
             child: Ink(
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(25, 167, 86, 0),
-                  borderRadius: BorderRadius.circular(16.0),
-                  border: Border.all(
-                    width: 4.0,
-                    color: const Color.fromARGB(255, 167, 86, 0),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 6.0,
-                        color: const Color.fromARGB(255, 167, 86, 0)
-                            .withOpacity(0.2),
-                        blurStyle: BlurStyle.outer),
-                  ]),
-              child: ScaleTransition(
-                alignment: Alignment.center,
-                scale: _scaleAnimationController,
-                child: AnimatedBuilder(
-                  animation: _imageColorAnimationController,
-                  builder: (context, child) {
-                    return Center(
-                      child: Image.asset(
-                        "$imagePath${widget.category}.png",
-                        fit: BoxFit.fitHeight,
-                        color: Color.fromARGB(
-                          255,
-                          0,
-                          60,
-                          (255 *
-                                  (_imageColorAnimationController.value - 1)
-                                      .abs())
-                              .floor(),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(25, 167, 86, 0),
+                    borderRadius: BorderRadius.circular(16.0),
+                    border: Border.all(
+                      width: 4.0,
+                      color: const Color.fromARGB(255, 167, 86, 0),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 6.0,
+                          color: const Color.fromARGB(255, 167, 86, 0)
+                              .withOpacity(0.2),
+                          blurStyle: BlurStyle.outer),
+                    ]),
+                child: CategoryTransitionWidget(
+                  isHighlight: widget.isHighlight,
+                  duration: const Duration(seconds: 1),
+                  imagePath: "$imagePath${widget.category}.png",
+                )),
           ),
         ),
         Padding(
@@ -128,12 +75,5 @@ class _CategoryState extends State<Category> with TickerProviderStateMixin {
 
   Future<List<Entry>> getEntries() async {
     return await apiController.getEntriesByCategory(category: widget.category);
-  }
-
-  @override
-  void dispose() {
-    _scaleAnimationController.dispose();
-    _imageColorAnimationController.dispose();
-    super.dispose();
   }
 }
---
+++ lib/screens/components/category_transition.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class CategoryTransitionWidget extends StatefulWidget {
+  final bool isHighlight;
+  final Duration duration;
+  final String imagePath;
+  const CategoryTransitionWidget({
+    super.key,
+    required this.isHighlight,
+    required this.duration,
+    required this.imagePath,
+  });
+
+  @override
+  State<CategoryTransitionWidget> createState() =>
+      _CategoryTransitionWidgetState();
+}
+
+class _CategoryTransitionWidgetState extends State<CategoryTransitionWidget>
+    with TickerProviderStateMixin {
+  late AnimationController _scaleAnimationController;
+  late AnimationController _imageColorAnimationController;
+
+  @override
+  void initState() {
+    _scaleAnimationController = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+      lowerBound: 0.8,
+      upperBound: 1,
+    );
+
+    _imageColorAnimationController = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    if (widget.isHighlight) {
+      _scaleAnimationController.repeat(
+        reverse: true,
+      );
+      _imageColorAnimationController.repeat(
+        reverse: true,
+      );
+    } else {
+      _scaleAnimationController.animateTo(1);
+      _imageColorAnimationController.animateTo(0);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CategoryTranstion(
+      imageColorAnimationController: _imageColorAnimationController,
+      scaleAnimationController: _scaleAnimationController,
+      imagePath: widget.imagePath,
+    );
+  }
+
+  @override
+  void dispose() {
+    _scaleAnimationController.dispose();
+    _imageColorAnimationController.dispose();
+    super.dispose();
+  }
+}
+
+class CategoryTranstion extends AnimatedWidget {
+  final Animation<double> scaleAnimationController;
+  final Animation<double> imageColorAnimationController;
+  final String imagePath;
+
+  const CategoryTranstion({
+    super.key,
+    required this.imageColorAnimationController,
+    required this.scaleAnimationController,
+    required this.imagePath,
+  }) : super(
+          listenable: imageColorAnimationController,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      alignment: Alignment.center,
+      scale: scaleAnimationController,
+      child: Center(
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.fitHeight,
+          color: Color.fromARGB(
+            255,
+            0,
+            60,
+            (255 * (imageColorAnimationController.value - 1).abs()).floor(),
+          ),
+        ),
+      ),
+    );
+  }
+}
